9e83f18f28cda1ac56d6fa29236e7fe5af9db58f
Gerdzioo <52524132+Gerdzioo@users.noreply.github.com>
2022-01-03 10:19:31 +0100

    master

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-
 <html lang="pl">
 
 
 
 <head>
 
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
 
     <meta name="description" content="hack game PL">
 
@@ -39,96 +38,137 @@
 
 <body>
 
-<?php
+    <?php
 
-include("php/db.php");
+    include("php/db.php");
 
     session_start();
 
-?>
+    ?>
 
-<div id='gora'>
+    <div id='gora'>
 
         <div id='panelLewa'>
 
-            <a href='index.php'><div class='text-gora font'><i class='fas fa-home'></i></div></a>
+            <a href='index.php'>
+                <div class='text-gora font'><i class='fas fa-home'></i></div>
+            </a>
 
         </div>
 
         <div id='panelPrawa'>
 
-             <div class='text-gora zaloguj'><i class='fas fa-sign-in-alt'></i><br><div class='small-text'>Zaloguj się</div></div>
+            <div class='text-gora zaloguj'><i class='fas fa-sign-in-alt'></i><br>
+                <div class='small-text'>Zaloguj się</div>
+            </div>
 
-             <div class='text-gora zarejestruj'><i class='fas fa-plus'></i><br><div class='small-text'>Zarejestruj się</div></div>
+            <div class='text-gora zarejestruj'><i class='fas fa-plus'></i><br>
+                <div class='small-text'>Zarejestruj się</div>
+            </div>
 
-             <div class='text-gora pomoc'><i class='fas fa-question'></i><br><div class='small-text'>Pomoc</div></div>
+            <div class='text-gora pomoc'><i class='fas fa-question'></i><br>
+                <div class='small-text'>Pomoc</div>
+            </div>
 
-             <div class='text-gora faq'><i class='fas fa-info'></i><br><div class='small-text'>FAQ</div></div>
-             <script>
-                 $(document).ready(function(){
-                  $(".zaloguj").click(function(){
+            <div class='text-gora faq'><i class='fas fa-info'></i><br>
+                <div class='small-text'>FAQ</div>
+            </div>
+            <script>
+            $(document).ready(function() {
+                $(".zaloguj").click(function() {
                     $(".panelloguj").show(1500);
-                  });
                 });
+            });
 
-                $(document).ready(function(){
-                  $(".zarejestruj").click(function(){
+            $(document).ready(function() {
+                $(".zarejestruj").click(function() {
                     $(".panelzarejestruj").show(1500);
-                  });
                 });
-             </script>
+            });
+            </script>
         </div>
 
         <div id='panelPrawaMobile'>
 
-            <a href='?page=panel'><div class='text-gora font'><i class='fas fa-bars'></i></div></a>
+            <div class='font rozwinMenu'><i class='fas fa-bars'></i></div>
 
+            <script>
+            $(document).ready(function() {
+                $(".rozwinMenu").click(function() {
+                    $(".panelmenu").show(1500);
+                    $(".rozwinMenu").hide(1200);
+                });
+            });
+            </script>
         </div>
-
-</div>
-<!--tu są ukryte elementy-->
-
-        <!--panel logowania-->
-            <div class="panelloguj">
-                <div class="wyjscieLogowanie">X</div>
-                <script>
-                    $(document).ready(function(){
-                     $(".wyjscieLogowanie").click(function(){
-                       $(".panelloguj").hide(1500);
-                     });
-                   });
-                </script>
-                <form action="php/zaloguj.php" method="POST">
-                    Login:<br>
-                    <input type="text" id="loginInput" name="login"><br>
-                    Hasło:<br>
-                    <input type="password" id="passwordInput" name="password"><br><br>
-                    <input type="submit" value="Zaloguj się">
-                </form>
+        <div class="panelmenu">
+            <div class="wyjscieMenu">X</div>
+            <div class='text-gora zaloguj'><i class='fas fa-sign-in-alt'></i><br>
+                <div class='small-text'>Zaloguj się</div>
             </div>
-            
-            <!--panel rejestracji-->
-            <div class="panelzarejestruj">
-                <div class="wyjscieRejestracja">X</div>
-                <script>
-                    $(document).ready(function(){
-                     $(".wyjscieRejestracja").click(function(){
-                       $(".panelzarejestruj").hide(1500);
-                     });
-                   });
-                </script>
-                <form action="php/zarejestruj.php" method="POST">
-                    Login:<br>
-                    <input type="text" id="loginInput" name="login"><br>
-                    Hasło:<br>
-                    <input type="password" id="passwordInput" name="password"><br>
-                    Email:<br>
-                    <input type="email" id="emailInput" name="email"><br>
-                    <input type="submit" value="Zarejestruj się">
-                </form>
+            <div class='text-gora zarejestruj'><i class='fas fa-plus'></i><br>
+                <div class='small-text'>Zarejestruj się</div>
             </div>
+            <div class='text-gora pomoc'><i class='fas fa-question'></i><br>
+                <div class='small-text'>Pomoc</div>
+            </div>
+            <div class='text-gora faq'><i class='fas fa-info'></i><br>
+                <div class='small-text'>FAQ</div>
+            </div>
+        </div>
+        <script>
+        $(document).ready(function() {
+            $(".wyjscieMenu").click(function() {
+                $(".panelmenu").hide(1200);
+                $(".rozwinMenu").show(1500);
+            });
+        });
+        </script>
 
-<!--koniec ukrytych elementów-->
+    </div>
+    <!--tu są ukryte elementy-->
+
+    <!--panel logowania-->
+    <div class="panelloguj">
+        <div class="wyjscieLogowanie">X</div>
+        <script>
+        $(document).ready(function() {
+            $(".wyjscieLogowanie").click(function() {
+                $(".panelloguj").hide(1500);
+            });
+        });
+        </script>
+        <form action="php/zaloguj.php" method="POST">
+            Login:<br>
+            <input type="text" id="loginInput" name="login"><br>
+            Hasło:<br>
+            <input type="password" id="passwordInput" name="password"><br><br>
+            <input type="submit" value="Zaloguj się">
+        </form>
+    </div>
+
+    <!--panel rejestracji-->
+    <div class="panelzarejestruj">
+        <div class="wyjscieRejestracja">X</div>
+        <script>
+        $(document).ready(function() {
+            $(".wyjscieRejestracja").click(function() {
+                $(".panelzarejestruj").hide(1500);
+            });
+        });
+        </script>
+        <form action="php/zarejestruj.php" method="POST">
+            Login:<br>
+            <input type="text" id="loginInput" name="login"><br>
+            Hasło:<br>
+            <input type="password" id="passwordInput" name="password"><br>
+            Email:<br>
+            <input type="email" id="emailInput" name="email"><br>
+            <input type="submit" value="Zarejestruj się">
+        </form>
+    </div>
+
+    <!--koniec ukrytych elementów-->
 
 
     <div id="leweOkno">
@@ -138,12 +178,16 @@ include("php/db.php");
             Hacker Game
 
         </div>
-    
+
         <br>
 
         <div class="just font-regular font-center">
 
-            <span style="color: white;">Chcesz zostać <span style="color: red;">hackerem</span> bez ponoszenia konsekwencji w życiu? Chcesz się bawić i zostać najlepszym z najlepszych lecz w dodatku niewykrywalnym? Zapraszam Cię tu do naszej społeczności! Tylko u nas znajdziesz taką rozgrywkę! <br><br> Podnoś się na naszej arenie hakerów i pokaż kto tu jest anonimowy!<br> Załóż kontku juz dzisiaj nie zwlekaj z tą decyzją!</span>
+            <span style="color: white;">Chcesz zostać <span style="color: red;">hackerem</span> bez ponoszenia
+                konsekwencji w życiu? Chcesz się bawić i zostać najlepszym z najlepszych lecz w dodatku niewykrywalnym?
+                Zapraszam Cię tu do naszej społeczności! Tylko u nas znajdziesz taką rozgrywkę! <br><br> Podnoś się na
+                naszej arenie hakerów i pokaż kto tu jest anonimowy!<br> Załóż kontku juz dzisiaj nie zwlekaj z tą
+                decyzją!</span>
 
         </div>
 
@@ -158,26 +202,25 @@ include("php/db.php");
             Zdjęcia z gry
 
         </div>
+        <br><br>
 
     </div>
 
 
 
-<?php
+    <?php
 
-if(isset($_SESSION['status'])){
+    if (isset($_SESSION['status'])) {
 
-            if($_SESSION['status']=="zalogowany"){
+        if ($_SESSION['status'] == "zalogowany") {
 
-                header('Location: pc.php');
-
-            }
-
+            header('Location: pc.php');
         }
-?>
+    }
+    ?>
 
 </body>
 
 
 
-</html> 
+</html>
